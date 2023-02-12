db2f9d1e3c5dc89dd8e452dd359e1f541ad0858c
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-12 17:41:37 +0530

    Added scripts

---
+++ 05-Adding-navigating-programmatically/src/pages/Home.js
@@ -0,0 +1,21 @@
+import { Link, useNavigate } from "react-router-dom";
+
+function HomePage() {
+
+    // used to switch to diff route from inside code
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        navigate('/products');
+    }; 
+    
+    return (
+        <>
+        <h1>My Home page</h1>
+        <p>go to prodcuts <Link to="/products">list</Link></p>
+        <p><button onClick={navigateHandler}>Navigate</button></p>
+        </>
+    );
+};
+
+export default HomePage;
