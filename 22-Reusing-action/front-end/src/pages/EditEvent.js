45a7fcb1b476cccb6e3d691d61fa7b306be99284
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-17 21:32:37 +0530

    Added sripts

---
+++ 22-Reusing-action/front-end/src/pages/EditEvent.js
@@ -0,0 +1,16 @@
+import EventForm from '../components/EventForm';
+import { useRouteLoaderData } from 'react-router-dom';
+
+function EditEventpage() {
+
+    const data = useRouteLoaderData('event-detail');
+    const event = data.event;
+
+    return (
+        <>
+           <EventForm event={event}/>
+        </>
+    );
+};
+
+export default EditEventpage;
---
+++ 22-Reusing-action/front-end/src/pages/NewEvent.js
@@ -0,0 +1,12 @@
+import EventForm from "../components/EventForm";
+
+function NewEventPage() {
+    return (
+        <>
+           <EventForm method='post'/>
+        </>
+    );
+};
+
+export default NewEventPage;
+
